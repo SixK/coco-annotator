b738b0244d24e486e5235ab701ec01f37568863a
SixK <dasixk@free.fr>
2023-04-05 00:54:03 +0200

    Rewrite store with vuex4 style

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,9 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import { createStore } from 'vuex';
 
 import user from "./user";
 import info from "./info";
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
+export default createStore({
   modules: {
     user,
     info
